8bcdee13f4ce1fb68d57f836a645b301f2d7245f
kent.liu.philippines@gmail.com <liu947466968>
2024-12-09 18:08:43 +0800

    1

--- src/main/java/com/site/config/SQLiteDialectConfig.java
+++ src/main/java/com/site/config/SQLiteDialectConfig.java
@@ -1,6 +1,5 @@
 package com.site.config;
 
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
