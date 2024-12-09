e9e5fc6bce7eccc10aed4362b8642c6ce4482c04
kent.liu.philippines@gmail.com <liu947466968>
2024-12-10 02:29:07 +0800

    11

--- src/main/java/com/site/config/HttpsConfig.java
+++ src/main/java/com/site/config/HttpsConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.http11.Http11NioProtocol;
 import org.apache.catalina.Context;
+import org.springframework.core.io.Resource;
 
 @Slf4j
 @Configuration
@@ -19,16 +20,23 @@ public class HttpsConfig {
 
     @Value("${server.port:443}")
     private int httpsPort;
+    
+    @Value("${ssl.cert-path:certs}")
+    private String certPath;
 
     @Bean
     public ServletWebServerFactory servletContainer() {
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
             @Override
             protected void postProcessContext(Context context) {
-                // 可以在这里添加其他上下文配置
+                // 禁用SSL配置，使用动态配置
+                context.setUseHttpOnly(true);
             }
         };
-
+        
+        // 设置SSL为false，使用动态配置
+        tomcat.setSsl(null);
+        
         // 添加HTTP连接器
         tomcat.addAdditionalTomcatConnectors(createStandardConnector());
         
@@ -41,6 +49,16 @@ public class HttpsConfig {
         connector.setSecure(false);
         connector.setScheme("http");
         connector.setRedirectPort(httpsPort);
+        
+        // 配置连接器参数
+        connector.setProperty("relaxedPathChars", "[]|");
+        connector.setProperty("relaxedQueryChars", "[]|{}^&#x5c;&#x60;&lt;&gt;");
+        connector.setProperty("maxThreads", "200");
+        connector.setProperty("acceptCount", "100");
+        connector.setProperty("maxConnections", "10000");
+        connector.setProperty("connectionTimeout", "20000");
+        connector.setProperty("maxHttpHeaderSize", "8192");
+        
         return connector;
     }
 } 
