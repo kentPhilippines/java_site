082d0f04c2a39b0f114ddab04c90311a5ba3b6ca
kent.liu.philippines@gmail.com <liu947466968>
2024-12-09 13:35:17 +0800

    1

--- src/main/java/com/site/repository/SiteConfigRepository.java
+++
@@ -1,8 +0,0 @@
-package com.site.repository;
-
-import com.site.entity.SiteConfig;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SiteConfigRepository extends JpaRepository<SiteConfig, Long> {
-    SiteConfig findByKey(String key);
-} 
